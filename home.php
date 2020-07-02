77155cafa6ce9cf4732061ff5a29ca3ad4bc444c
ttailslife1562 <54633274+ttailslife1562@users.noreply.github.com>
2020-07-01 20:52:13 -0500

    Add files via upload

--- home.php
+++ home.php
@@ -1,57 +1,35 @@
-<!-- <h1><?php echo $title;?></h1> -->
-
-<div class="col-lg-12">  
-<h4>Politecnico Internacional</h4>
-    <p>Calle 73 10 45 Bogota Colombia</p> 
-</div>
-<div  class="col-lg-6">
-  <h4>Vision</h4> 
-  <p>Center of Academic Excellence Delivering Quality Service to all.</p>
-  <h4>Mission</h4>
-  <p> Capiz State University is committed to provide advanced knowledge and innovation; develop skills, talents and values; undertake relevant research; development and extension services; promote entrepreneurship and environmental consciousness; and enhance industry collaboration  & linkages with partner agencies.</p>
-  <h4>Goals</h4> 
-  <ol>
-    <li>Globally competitive graduates</li>  
-    <li>Institutionalize research culture</li> 
-    <li>Responsive and sustainable extension services</li> 
-    <li>Maximized profit of variable agro-industrial business ventures</li>  
-    <li>Effective and efficient administration </li>  
-  </ol>
-
-  <h4>Objectives</h4> 
-   <ol>
-      <li>Create an environment of shared leadership and responsibilities with competent administrator.</li>
-      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
-      <li>Produce highly competitive graduates.</li>
-      <li>Conduct relevant and updated researches</li>
-      <li>Extend financial support and manpower for outreach activities.</li> 
-   </ol> 
-</div>
-<div  class="col-lg-6">
-  <h4>Guiding Principles</h4> 
-    <ul>
-      <li>Academic Freedom </li>
-      <li>Responsibility</li>
-      <li> Academic Standards</li>  
-    </ul>
-<h4>Core Values</h4>
-  <ul>
-    <li>God-Centered</li>
-    <li>Excellence</li>
-    <li>Integrity</li>
-    <li>Transparency and Accountability</li>
-    <li>Dedication to Quality Service</li>  
-  </ul> 
-<h4>Quality Policy</h4> 
-<ol>
-    <li>CAPSU is committed to be the center of academic excellence delivering quality service to all by;</li> 
-    <li>Continuing innovations and quality improvement cultivating an efficient and effective and environment for maximum clientele satisfaction;</li> 
-    <li>Adhering to laws and regulations, global standards, and environmental change requirements;</li> 
-    <li>Participating in sustainable developments projects for inclusive economic for inclusive 
-economic growth;</li> 
-    <li>Showcasing quality outputs; and,</li> 
-    <li>Upholding values and integrity and nurturing talents and skills for global competitiveness.</li>  
-</ol>
-</div>
+<style type="text/css">
  
+  .secondrow > .row {
+     border: 1px solid #ddd;
+    /*padding: 5px 30px;*/
+    text-align: center; 
+    margin: 20px;
+    min-height: 50px;
+     border-radius: 50%;
+  }
+  .imgstretch{ 
+    padding: 10px 30px;
+  }
+  .imgstretch img {
+    width: 100%;
+    height: 100px;
+  }
 
+ 
+</style>
+
+  <div class="btn-controls">
+    <div class="btn-box-row row-fluid">
+        <a href="#" class="btn-box big span4"><i class=" icon-random"></i><b>65%</b>
+            <p class="text-muted">
+                Lecciones</p>
+        </a><a href="#" class="btn-box big span4"><i class="icon-user"></i><b>15</b>
+            <p class="text-muted">
+                Usuarios</p>
+        </a><a href="#" class="btn-box big span4"><i class="icon-money"></i><b>15,152</b>
+            <p class="text-muted">
+                Ejercicios</p>
+        </a>
+    </div> 
+</div>
--- index.php
+++ index.php
@@ -1,55 +1,30 @@
-<?php 
-require_once("include/initialize.php");  
-if (!isset($_SESSION['StudentID'])) {
-  # code...
-  redirect('login.php');
+<?php
+require_once("../../../include/initialize.php");
+if(!isset($_SESSION['USERID'])){
+	redirect(web_root."admin/index.php");
 }
-$content='home.php';
-$view = (isset($_GET['q']) && $_GET['q'] != '') ? $_GET['q'] : '';
-switch ($view) { 
-  case 'lesson':
-    $title = "Lesson";
-    $content = 'lesson.php';
-   # code...
-   break; 
-  case 'exercises':
-    $title = "Exercises";
-    $content = 'exercises.php';
-   # code...
-   break; 
-  case 'download':
-    $title = "Download";
-    $content = 'download.php';
-   # code...
-   break; 
-  case 'about':
-    $title = "About Us";
-    $content = 'about.php';
-   # code...
-   break; 
-  case 'playvideo':
-    $title = "Play Video";
-    $content = 'playvideo.php';
-   # code...
-   break; 
-  case 'viewpdf':
-    $title = "Play Video";
-    $content = 'viewpdf.php';
-   # code...
-   break; 
-  case 'question':
-    $title = "Question";
-    $content = 'question.php';
-   # code...
-   break; 
-  case 'quizresult':
-    $title = "Result";
-    $content = 'quizresult.php';
-   # code...
-   break; 
-  default :
-    $title = "Home";
-    $content    = 'home.php';
+
+$view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
+
+switch ($view) {
+	case 'list' :
+		$content    = 'list.php';		
+		break;
+
+	case 'add' :
+		$content    = 'add.php';		
+		break;
+
+	case 'edit' :
+		$content    = 'edit.php';		
+		break;
+    case 'view' :
+		$content    = 'view.php';		
+		break;
+
+	default :
+		$content    = 'list.php';		
 }
-require_once("theme/templates.php");
+require_once("../../themes/templates.php");
 ?>
+  
